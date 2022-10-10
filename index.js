c23fb4f598c902b4c349dd4ddfd36c47edc8da62
Saif Resun <saif.resun@outlook.com>
2022-10-10 15:20:59 +0600

    redesigned

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-function goat(id) {
-	let tag = document.getElementById(id);
+window.onload = async () => {
+	await sleep(500);
+	await tw_print(100, "_resun", document.getElementById("title"), true);
+	await sleep(500);
+	await tw_print(100, "hello, world\\n", document.getElementById("greating_text"));
+	await sleep(500);
+	await tw_print(50, "I am Saif. I love the C programming language.", document.getElementById("about_me"));
+	
+	await sleep(500);
+	document.getElementById("account_links").className += " active";
+}
+document.onmouseup = change_selected_text_color;
+document.onkeyup = change_selected_text_color;
+const root = document.querySelector(':root');
+const selected_text_colors = [
+	"#4dff4d",	// green
+	"#ffeb3b",	// yellow
+	"#1e90ff",	// dodger blue
+	"#f44336",	// red
+	"#00ffff",	// azure
+]
+const selected_text_colors_len = selected_text_colors.length;
+function change_selected_text_color() {
+	let i = Math.floor(Math.random() * (selected_text_colors_len));
+	root.style.setProperty("--selected_text_color", selected_text_colors[i]);
+}
+function goat(tag) {
 	window.scroll({
 		top: tag.offsetTop,
 		left: tag.offsetLeft,
 		behavior: 'smooth'
 	});
 }
-	
+function goat_id(id) {
+	let tag = document.getElementById(id);
+	goat(tag);
+}
 function col_exp(tag, rtt_tag) {
 	taggle_state(tag);
 	rtt_tag.classList.toggle("_rotation_active");
@@ -41,7 +69,7 @@ function taggle_state(tag) {
 		tag.classList.add("active");
 	}
 }
-async function tw_print(s, tag) {
+async function tw_print(ms, s, tag, keep_cursor=false) {
 	const s_len = s.length;
 	tag.innerHTML = "";
 
@@ -54,11 +82,13 @@ async function tw_print(s, tag) {
 	tag.appendChild(cursor);
 
 	for (let i = 0; i < s_len; i++) {
-		await sleep(100);
+		await sleep(ms);
 		text.innerText += s[i];
 	}
+	if (!keep_cursor) {
+		cursor.remove();
+	}
 }
-		
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
